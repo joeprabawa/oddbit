c42305c7a2781870cfeca933c51594a1deef5a98
unknown <joeprabawa@gmail.com>
2020-01-18 22:20:09 +0800

    fix search by date

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ const BASE_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}
 const GENRE_URL = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`;
 const POSTER_URL = "https://image.tmdb.org/t/p/w500";
 
-let order = false;
-let dateStore = [];
-
 const selector = document.querySelector("#results");
 const sortButton = document.querySelectorAll(".sort");
 const icon = document.querySelectorAll(".icon");
-const dates = document.querySelectorAll(".datepicker");
+const form = document.querySelector("form");
+const { elements } = form;
 const caption = document.querySelector("#caption");
+let order = false;
 
 fetch(BASE_URL)
   .then(data => data.json())
@@ -40,22 +39,22 @@ fetch(BASE_URL)
     });
 
     // Render results berdasarkan date range
-    dates.forEach(date => {
-      date.addEventListener("change", e => {
-        dateStore.push(e.target.value);
-        if (dateStore.length > 2) return (dateStore = []);
-        const start = dateStore[0];
-        const end = dateStore[1];
-        console.log(start, end);
-        const eachDay = dateFns.eachDay(start, end);
-        const newFormat = eachDay.map(v => {
-          return dateFns.format(v, [(format = "YYYY-MM-DD")]);
-        });
-        renderToPage(results.filter(v => newFormat.includes(v.release_date)));
+    form.addEventListener("submit", e => {
+      e.preventDefault();
+      const start = [elements["start"].value];
+      const end = [elements["end"].value];
+
+      const eachDay = dateFns.eachDay(start[0], end[0]);
+      const newFormat = eachDay.map(v => {
+        return dateFns.format(v, [(format = "YYYY-MM-DD")]);
       });
+      renderToPage(results.filter(v => newFormat.includes(v.release_date)));
+
+      form.reset();
     });
   });
 
+// Render reults, dan masukan ke dalam DOM
 function renderToPage(results) {
   const reducer = results.map(async val => {
     const genreResult = await getGenre(val.genre_ids);
@@ -71,6 +70,9 @@ function renderToPage(results) {
     }">
     </div>
     <div class="card-content">
+    <p>${dateFns.format(new Date(val.release_date), [
+      (format = "d MMM YYYY")
+    ])}</p>
   <span class="card-title activator grey-text text-darken-4">${truncate(
     val.original_title,
     15
@@ -90,8 +92,11 @@ function renderToPage(results) {
         </div>`);
       }, "")}
       <hr>
-       <h5>Popularity | Vote Count</h5>
-       ${val.popularity} ~ ${val.vote_count}
+       <h5>Popularity</h5>
+       ${val.popularity}
+       <hr>
+       <h5>Vote Count</h5>
+       ${val.vote_count}
     </div>
     </div>
     </div>
