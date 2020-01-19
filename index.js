53ff0989d8b690eb02293ed8b5cbd180cae45b0c
joe_prabawa <joeprabawa@gmail.com>
2020-01-20 02:17:20 +0800

    add toggle list view

--- index.js
+++ index.js
@@ -7,14 +7,22 @@ const selector = document.querySelector("#results");
 const sortButton = document.querySelectorAll(".sort");
 const icon = document.querySelectorAll(".icon");
 const form = document.querySelector("form");
-const { elements } = form;
+const thumbControl = document.querySelector(".thumb");
 const caption = document.querySelector("#caption");
+const toggleIcon = document.querySelector(".toggle-icon");
+
+const { elements } = form;
 let order = false;
+let thumb = false;
 
 fetch(BASE_URL)
   .then(data => data.json())
   .then(({ results }) => {
     renderToPage(results);
+    thumbControl.addEventListener("click", e => {
+      thumb = !thumb;
+      renderToPage(results);
+    });
 
     // Render results berdasarkan tombol sorting
     sortButton.forEach(button => {
@@ -49,25 +57,51 @@ fetch(BASE_URL)
         return dateFns.format(v, [(format = "YYYY-MM-DD")]);
       });
       renderToPage(results.filter(v => newFormat.includes(v.release_date)));
-
       form.reset();
     });
   });
 
 // Render reults, dan masukan ke dalam DOM
 function renderToPage(results) {
+  thumb
+    ? (toggleIcon.innerHTML = "view_agenda")
+    : (toggleIcon.innerHTML = "dashboard");
+
   const reducer = results.map(async val => {
     const genreResult = await getGenre(val.genre_ids);
     caption.innerHTML = `${results.length} ${
       results.length > 1 ? "Results" : "Result"
     } found`;
-    return `
-   <div class="col s12 m4">
-    <div class="card ">
+    return !thumb
+      ? `
+      <ul class="collection">
+          <li class="collection-item avatar">
+            <div class="img-avatar">
+              <img  class="circle" style="object-fit:cover;width:60px;height:60px;margin-top:10px" src="${POSTER_URL}${
+          val.poster_path
+        }">
+        </div>
+        <div class="content">
+        <span class="title">${truncate(val.original_title, 40)}</span>
+          <p>${dateFns.format(new Date(val.release_date), [
+            (format = "d MMM YYYY")
+          ])} 
+            <br>
+              ${genreResult.reduce((acc, v) => {
+                return (acc += `<div class="chip list">
+          ${v}
+          </div>`);
+              }, "")}
+          </p>
+        </div>
+        </li>
+      </ul>`
+      : `<div class="col s12 m4">
+    <div class="card">
     <div class="card-image waves-effect waves-block waves-light">
       <img style="height:375px;object-fit:cover" class="activator" src="${POSTER_URL}${
-      val.poster_path
-    }">
+          val.poster_path
+        }">
     </div>
     <div class="card-content">
     <p>${dateFns.format(new Date(val.release_date), [
@@ -75,7 +109,7 @@ function renderToPage(results) {
     ])}</p>
   <span class="card-title activator grey-text text-darken-4">${truncate(
     val.original_title,
-    15
+    10
   )}<i class="material-icons right">more_vert</i></span>
     </div>
     <div class="card-reveal">
@@ -102,7 +136,33 @@ function renderToPage(results) {
     </div>
      `;
   });
+
+  function getTracks() {
+    //Append playlist name to modal header
+    const modalTitle = document.querySelector("h4.title");
+    const buttons = document.querySelectorAll("a.modal-trigger");
+    buttons.forEach(button => {
+      button.addEventListener("click", e => {
+        console.log("clicked");
+        // Append Playlist name to modal title
+        modalTitle.innerHTML = button.dataset.title;
+        // Get the playlist url
+        const url = button.dataset.link;
+        const options = {
+          headers: {
+            Authorization: `Bearer ${_token}`
+          }
+        };
+        // Fetch playlist url
+        fetch(url, options)
+          .then(response => response.json())
+          .then(data => appendToModal(data));
+      });
+    });
+  }
+
   Promise.all(reducer).then(values => {
+    getTracks();
     selector.innerHTML = values.join("");
   });
 }
@@ -123,3 +183,10 @@ function getGenre(arr) {
 function truncate(str, length) {
   return str.length > length ? `${str.substring(0, length)} ..` : str;
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  var elems = document.querySelectorAll(".datepicker");
+  var instances = M.Datepicker.init(elems, {
+    format: "yyyy-mm-dd"
+  });
+});
